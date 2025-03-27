10b52ec62a3a5c75e87cda441de307ac02eb39fe
Paulo Soek <167770189+Soneze@users.noreply.github.com>
2025-03-27 15:42:18 -0300

    Atualiza configurações de build e documentação

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Busca-de-Perfil-no-GitHub/',
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 }) 
